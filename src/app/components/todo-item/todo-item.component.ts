457927fb95a84675920f552b192c2b1a73de42c9
Tylyps <j.chmielewski@o2.pl>
2019-06-01 14:59:19 +0200

    Add service and loading from http

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TodoService } from '../../services/todo.service'
 
 import { Todo } from "../../models/Todo";
 @Component({
@@ -9,9 +10,30 @@ import { Todo } from "../../models/Todo";
 export class TodoItemComponent implements OnInit {
   @Input() todo:Todo;
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
+  }
+
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed,
+    }
+
+    return classes;
+  }
+
+  onToggle(todo) {
+    // Toggle in UI
+    todo.completed = !todo.completed;
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
+  }
+
+  onDelete(todo) {
+    console.log('delete');
   }
 
 }
